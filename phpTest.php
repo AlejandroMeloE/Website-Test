91cb093f75e7701d3720ad905f37deb1c7aaf84e
AlejandroMeloE <44786220+AlejandroMeloE@users.noreply.github.com>
2019-01-10 11:17:31 -0800

    login.php
    added the last part

--- phpTest.php
+++ phpTest.php
@@ -2,7 +2,7 @@
 $host = "127.0.0.1";
 $user = "alejandro0melo"
 $pass = "";
-$db = "WEBPROJECT.html";
+$db = "sample_db";
 $port = 3306;
 $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
 $query = "SELECT * FROM users";
@@ -11,3 +11,4 @@ while ($row = msqli_fetch_assoc($result)) {
 	echo "The ID is: " . $row['id']
  . " and the Username is: " . $row['username'];
 }
+?>
